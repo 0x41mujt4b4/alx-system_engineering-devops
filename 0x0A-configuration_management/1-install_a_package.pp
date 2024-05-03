5e4d59ed4d961f276b6b6216f9ae56da734eff9b
0x41mujt4b4 <almujtaba007@gmail.com>
2024-05-03 15:37:17 -0700

    update install package manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
+#!/usr/bin/pup
 # This Puppet manifest install flask version 2.1.0
-exec { 'install flask 2.1.0':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 freeze | grep flask==2.1.0',
-  path    => ['/bin/', '/usr/bin/'],
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
