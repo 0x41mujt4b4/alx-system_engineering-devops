59207f4d4cfa94f0353e26b001261042a4863c35
Almujtaba Abdalla <almujtaba007@gmail.com>
2024-07-29 06:21:07 -0700

    add 0x15 api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+python module to send api request to todo list api data
+"""
+import requests
+from sys import argv
+
+id = ''
+try:
+    id = int(argv[1])
+except Exception:
+    id = ''
+
+todo_url = f"https://jsonplaceholder.typicode.com/todos"
+user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+
+todo_res = requests.get(url=todo_url)
+user_res = requests.get(url=user_url)
+
+todo_data = todo_res.json()
+user_data = user_res.json()
+
+NUMBER_OF_DONE_TASKS = 0
+TOTAL_NUMBER_OF_TASKS = 0
+for task in todo_data:
+    if task['userId'] == id:
+        TOTAL_NUMBER_OF_TASKS += 1
+    if task['userId'] == id and task['completed']:
+        NUMBER_OF_DONE_TASKS += 1
+print(f"Employee {user_data['name']} is done with tasks", end='')
+print(f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+for task in todo_data:
+    if task['userId'] == id and task['completed']:
+        print(f"\t {task['title']}")
